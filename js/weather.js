bcc3b122e67c8e22d778e2c71192b6f6c9f2e520
jadilovic <adilovic79@yahoo.com>
2021-03-05 16:01:24 +0100

    weather page created

---
+++ js/weather.js
@@ -0,0 +1,16 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+window.addEventListener("load", ()=>{
+	let long;
+	let lat;
+	
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+		});
+	}
+});
