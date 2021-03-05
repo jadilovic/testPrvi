f3b2eaf0fc084a0a6796382d42dcbd530b185967
jadilovic <adilovic79@yahoo.com>
2021-03-05 23:03:23 +0100

    new weather

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # testPrvi
 Ponavljanje kreiranja prvog projekta na githubu
 Dopunjen readMe.md
+
+Weather tutorial
+https://www.youtube.com/watch?v=wPElVpR1rwA
+
+weather api
+https://openweathermap.org/
+
--- js/weather.js
+++ js/weather.js
@@ -11,6 +11,17 @@ window.addEventListener("load", ()=>{
             console.log(position);
 			long = position.coords.longitude;
 			lat = position.coords.latitude;
+
+			const proxy = "https://cors-anywhere.herokuapp.com/"
+			const api = `${proxy}https://api.weatherapi.com/v1/forecast.xml?key=a1e24f88992a42a882b185946210503&q=${lat}/${long}&days=1&aqi=no&alerts=no`;
+		
+			fetch(api)
+				.then(response =>{
+					return response.json();
+				})
+				.then(data =>{
+				console.log(data);
+			});
 		});
 	}
 });
